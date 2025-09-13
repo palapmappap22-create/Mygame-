1f3fc201d3ba477f42ab4d4241fe0a959392b417
palapmappap22-create <palapmappap22@gmail.com>
2025-09-13 10:11:46 +0300

    Delete unity-builder.sh

--- unity-builder.sh
+++
@@ -1,21 +0,0 @@
-#!/bin/bash
-set -e
-
-# دالة لبناء المشروع
-build_project() {
-  echo "🔄 المحاولة مع Unity version: $1"
-  unity-editor -projectPath . -buildTarget Android -quit -batchmode -nographics -logFile build.log     -executeMethod BuildScript.BuildAndroid || return 1
-}
-
-# قائمة نسخ Unity المحتملة (يختار أي وحدة متاحة)
-UNITY_VERSIONS=("2022.3.0f1" "2021.3.0f1" "2020.3.0f1")
-
-for version in "${UNITY_VERSIONS[@]}"; do
-  if build_project "$version"; then
-    echo "✅ تم البناء بنجاح باستخدام Unity $version"
-    exit 0
-  fi
-done
-
-echo "❌ فشل البناء بكل النسخ المتاحة"
-exit 1
